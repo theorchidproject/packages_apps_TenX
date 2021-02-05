e723b9195d3c97a7545c300546e24a2bf2f3949f
Advaith Bhat <advaithbhat9@gmail.com>
2021-02-05 16:45:53 +0000

    TenX: [SQUASHED] Cleanup Theming
    Revert "TenX: Add MaterialOcean System Theme [3/3]"
    
    This reverts commit fad18753875c49f1f490b7d058b6f71e3de3b1c4.
    
    Revert "TenX: Add DarkGrey System Theme [3/3]"
    
    This reverts commit b17c4d998c450b311d2890d5dd3faefb17b04339.
    
    Revert "TenX: Add Reset Options to Themes"
    
    This reverts commit 6c8b040dea2dd99a7dc582a0943adb2a2d5bbb24.
    
    Revert "TenX: Add DU's PitchBlack Theme [3/3]"
    
    This reverts commit cf78ad2617a8e5613796467c7b7e38f9b4d7541a.
    
    Revert "TenX: Add ChocoX & BakedGreen [3/3]"
    
    This reverts commit 0b1cc3812caef679eddf96256ce80d2b601e7284.
    
    Revert "TenX: Add DU themes [3/3]"
    
    This reverts commit 2a57b7cd57f3870c3779ec9795e0ce81d67fdfcf.

--- src/com/tenx/settings/fragments/UserInterface.java
+++ src/com/tenx/settings/fragments/UserInterface.java
@@ -18,13 +18,10 @@ package com.tenx.settings.fragments;
 
 import com.android.internal.logging.nano.MetricsProto;
 
-import static android.os.UserHandle.USER_SYSTEM;
-
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
-import android.app.UiModeManager;
 import android.content.Context;
 import android.content.ContentResolver;
 import android.content.DialogInterface;
@@ -39,25 +36,16 @@ import android.os.ServiceManager;
 import android.os.UserHandle;
 import androidx.preference.ListPreference;
 import androidx.preference.Preference;
-import androidx.preference.PreferenceManager;
 import androidx.preference.PreferenceScreen;
 import androidx.preference.Preference.OnPreferenceChangeListener;
 import androidx.preference.SwitchPreference;
 import android.provider.Settings;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.MenuInflater;
-
 import com.android.settings.R;
 
 import java.util.Arrays;
 import java.util.HashSet;
-import java.util.Objects;
 
-import com.android.internal.util.tenx.ThemesUtils;
-import com.android.internal.util.tenx.tenxUtils;
 import com.android.settings.SettingsPreferenceFragment;
-import com.tenx.support.preferences.SystemSettingListPreference;
 import com.tenx.support.colorpicker.ColorPickerPreference;
 
 public class UserInterface extends SettingsPreferenceFragment implements
@@ -67,33 +55,20 @@ public class UserInterface extends SettingsPreferenceFragment implements
     private static final String ACCENT_COLOR_PROP = "persist.sys.theme.accentcolor";
     private static final String GRADIENT_COLOR = "gradient_color";
     private static final String GRADIENT_COLOR_PROP = "persist.sys.theme.gradientcolor";
-    private static final String PREF_THEME_SWITCH = "theme_switch";
-    private static final int MENU_RESET = Menu.FIRST;
-
-    static final int DEFAULT = 0xff1a73e8;
 
     private IOverlayManager mOverlayService;
-    private UiModeManager mUiModeManager;
-
     private ColorPickerPreference mThemeColor;
     private ColorPickerPreference mGradientColor;
-    private SystemSettingListPreference mThemeSwitch;
 
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
 
         addPreferencesFromResource(R.xml.tenx_settings_ui);
-
-        mUiModeManager = getContext().getSystemService(UiModeManager.class);
-
         mOverlayService = IOverlayManager.Stub
                 .asInterface(ServiceManager.getService(Context.OVERLAY_SERVICE));
-
         setupAccentPref();
         setupGradientPref();
-        setupThemeSwitchPref();
-        setHasOptionsMenu(true);
     }
 
     @Override
@@ -118,81 +93,6 @@ public class UserInterface extends SettingsPreferenceFragment implements
                  mOverlayService.reloadAssets("com.android.systemui", UserHandle.USER_CURRENT);
              } catch (RemoteException ignored) {
              }
-        } else if (preference == mThemeSwitch) {
-            String theme_switch = (String) objValue;
-            final Context context = getContext();
-            switch (theme_switch) {
-                case "1":
-                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_NO, ThemesUtils.SOLARIZED_DARK);
-                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_NO, ThemesUtils.BAKED_GREEN);
-                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_NO, ThemesUtils.CHOCO_X);
-                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_NO, ThemesUtils.PITCH_BLACK);
-                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_NO, ThemesUtils.DARK_GREY);
-                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_NO, ThemesUtils.MATERIAL_OCEAN);
-                    break;
-                case "2":
-                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.SOLARIZED_DARK);
-                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.BAKED_GREEN);
-                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.CHOCO_X);
-                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.PITCH_BLACK);
-                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.DARK_GREY);
-                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.MATERIAL_OCEAN);
-                    break;
-                case "3":
-                    handleBackgrounds(true, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.SOLARIZED_DARK);
-                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.BAKED_GREEN);
-                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.CHOCO_X);
-                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.PITCH_BLACK);
-                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.DARK_GREY);
-                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.MATERIAL_OCEAN);
-                    break;
-                case "4":
-                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.SOLARIZED_DARK);
-                    handleBackgrounds(true, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.BAKED_GREEN);
-                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.CHOCO_X);
-                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.PITCH_BLACK);
-                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.DARK_GREY);
-                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.MATERIAL_OCEAN);
-                    break;
-                case "5":
-                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.SOLARIZED_DARK);
-                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.BAKED_GREEN);
-                    handleBackgrounds(true, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.CHOCO_X);
-                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.PITCH_BLACK);
-                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.DARK_GREY);
-                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.MATERIAL_OCEAN);
-                    break;
-                case "6":
-                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.SOLARIZED_DARK);
-                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.BAKED_GREEN);
-                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.CHOCO_X);
-                    handleBackgrounds(true, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.PITCH_BLACK);
-                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.DARK_GREY);
-                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.MATERIAL_OCEAN);
-                    break;
-                case "7":
-                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.SOLARIZED_DARK);
-                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.BAKED_GREEN);
-                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.CHOCO_X);
-                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.PITCH_BLACK);
-                    handleBackgrounds(true, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.DARK_GREY);
-                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.MATERIAL_OCEAN);
-                    break;
-                case "8":
-                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.SOLARIZED_DARK);
-                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.BAKED_GREEN);
-                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.CHOCO_X);
-                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.PITCH_BLACK);
-                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.DARK_GREY);
-                    handleBackgrounds(true, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.MATERIAL_OCEAN);
-                        break;
-            }
-            try {
-                 mOverlayService.reloadAndroidAssets(UserHandle.USER_CURRENT);
-                 mOverlayService.reloadAssets("com.android.settings", UserHandle.USER_CURRENT);
-                 mOverlayService.reloadAssets("com.android.systemui", UserHandle.USER_CURRENT);
-             } catch (RemoteException ignored) {
-             }
         }
         return true;
     }
@@ -201,7 +101,7 @@ public class UserInterface extends SettingsPreferenceFragment implements
         mThemeColor = (ColorPickerPreference) findPreference(ACCENT_COLOR);
         String colorVal = SystemProperties.get(ACCENT_COLOR_PROP, "-1");
         int color = "-1".equals(colorVal)
-                ? DEFAULT
+                ? Color.WHITE
                 : Color.parseColor("#" + colorVal);
         mThemeColor.setNewPreviewColor(color);
         mThemeColor.setOnPreferenceChangeListener(this);
@@ -211,107 +111,10 @@ public class UserInterface extends SettingsPreferenceFragment implements
         mGradientColor = (ColorPickerPreference) findPreference(GRADIENT_COLOR);
         String colorVal = SystemProperties.get(GRADIENT_COLOR_PROP, "-1");
         int color = "-1".equals(colorVal)
-                ? DEFAULT
+                ? Color.WHITE
                 : Color.parseColor("#" + colorVal);
         mGradientColor.setNewPreviewColor(color);
         mGradientColor.setOnPreferenceChangeListener(this);
-    }
-
-    private void setupThemeSwitchPref() {
-        mThemeSwitch = (SystemSettingListPreference) findPreference(PREF_THEME_SWITCH);
-        mThemeSwitch.setOnPreferenceChangeListener(this);
-        if (tenxUtils.isThemeEnabled("com.android.theme.darkgrey.system")) {
-            mThemeSwitch.setValue("7");
-        } else if (tenxUtils.isThemeEnabled("com.android.theme.pitchblack.system")) {
-            mThemeSwitch.setValue("6");
-        } else if (tenxUtils.isThemeEnabled("com.android.theme.materialocean.system")) {
-            mThemeSwitch.setValue("8");
-        } else if (tenxUtils.isThemeEnabled("com.android.theme.chocox.system")) {
-            mThemeSwitch.setValue("5");
-        } else if (tenxUtils.isThemeEnabled("com.android.theme.bakedgreen.system")) {
-            mThemeSwitch.setValue("4");
-        } else if (tenxUtils.isThemeEnabled("com.android.theme.solarizeddark.system")) {
-            mThemeSwitch.setValue("3");
-        } else if (mUiModeManager.getNightMode() == UiModeManager.MODE_NIGHT_YES) {
-            mThemeSwitch.setValue("2");
-        } else {
-            mThemeSwitch.setValue("1");
-        }
-    }
-
-    private void handleBackgrounds(Boolean state, Context context, int mode, String[] overlays) {
-        if (context != null) {
-            Objects.requireNonNull(context.getSystemService(UiModeManager.class))
-                    .setNightMode(mode);
-        }
-        for (int i = 0; i < overlays.length; i++) {
-            String background = overlays[i];
-            try {
-                mOverlayService.setEnabled(background, state, USER_SYSTEM);
-            } catch (RemoteException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    @Override
-    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-        menu.add(0, MENU_RESET, 0, R.string.reset)
-                .setIcon(R.drawable.ic_menu_reset)
-                .setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case MENU_RESET:
-                resetToDefault();
-                return true;
-            default:
-                return super.onContextItemSelected(item);
-        }
-    }
-
-    private void resetToDefault() {
-        AlertDialog.Builder alertDialog = new AlertDialog.Builder(getActivity());
-        alertDialog.setTitle(R.string.theme_option_reset_title);
-        alertDialog.setMessage(R.string.theme_option_reset_message);
-        alertDialog.setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int id) {
-                resetValues();
-            }
-        });
-        alertDialog.setNegativeButton(R.string.cancel, null);
-        alertDialog.create().show();
-    }
-
-    private void resetValues() {
-        final Context context = getContext();
-        mThemeSwitch = (SystemSettingListPreference) findPreference(PREF_THEME_SWITCH);
-        handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_NO, ThemesUtils.SOLARIZED_DARK);
-        handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_NO, ThemesUtils.BAKED_GREEN);
-        handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_NO, ThemesUtils.CHOCO_X);
-        handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_NO, ThemesUtils.PITCH_BLACK);
-        handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_NO, ThemesUtils.DARK_GREY);
-        handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_NO, ThemesUtils.MATERIAL_OCEAN);
-        setupThemeSwitchPref();
-        mGradientColor = (ColorPickerPreference) findPreference(GRADIENT_COLOR);
-        SystemProperties.set(GRADIENT_COLOR_PROP, "-1");
-	mGradientColor.setNewPreviewColor(DEFAULT);
-        mThemeColor = (ColorPickerPreference) findPreference(ACCENT_COLOR);
-	SystemProperties.set(ACCENT_COLOR_PROP, "-1");
-        mThemeColor.setNewPreviewColor(DEFAULT);
-        try {
-             mOverlayService.reloadAndroidAssets(UserHandle.USER_CURRENT);
-             mOverlayService.reloadAssets("com.android.settings", UserHandle.USER_CURRENT);
-             mOverlayService.reloadAssets("com.android.systemui", UserHandle.USER_CURRENT);
-        } catch (RemoteException ignored) {
-        }
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
     }
 
     @Override
