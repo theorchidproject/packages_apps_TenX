b0dfc7c124b3c0bd4f8d9703e398cfd872d5bf7d
theorchidproject <84352313+theorchidproject@users.noreply.github.com>
2022-10-09 16:50:15 +0100

    revert TenX: Use Master Switch for Battery Bar [2/2]
    reverts 79e0c3235af3efb69a0f62654ae610029bef4379

--- src/com/tenx/settings/fragments/StatusBar.java
+++ src/com/tenx/settings/fragments/StatusBar.java
@@ -40,17 +40,11 @@ public class StatusBar extends SettingsPreferenceFragment implements
     private static final String STATUS_BAR_LOGO = "status_bar_logo";
     private static final String CARRIER_LABEL = "carrier_label_enabled";
     private static final String STATUS_BAR_CLOCK = "status_bar_clock";
-    private static final String BATTERY_BAR_ENABLED = "battery_bar_enabled";
-    private static final String DUAL_STATUSBAR_ROW_MODE = "statusbar_dual_row";
-    private static final String DUAL_ROW_DATAUSAGE = "dual_row_datausage";
 
     private SystemSettingMasterSwitchPreference mNetworkTraffic;
     private SystemSettingMasterSwitchPreference mStatusBarLogo;
     private SystemSettingMasterSwitchPreference mCarrierLabel;
     private SystemSettingMasterSwitchPreference mStatusBarClockShow;
-    private SystemSettingMasterSwitchPreference mBatteryBar;
-    private SystemSettingListPreference mStatusbarDualRowMode;
-    private SystemSettingListPreference mDualRowDataUsageMode;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -78,53 +72,6 @@ public class StatusBar extends SettingsPreferenceFragment implements
         mStatusBarClockShow.setChecked((Settings.System.getInt(resolver,
                 Settings.System.STATUS_BAR_CLOCK, 1) == 1));
         mStatusBarClockShow.setOnPreferenceChangeListener(this);
-
-        mBatteryBar = (SystemSettingMasterSwitchPreference) findPreference(BATTERY_BAR_ENABLED);
-        mBatteryBar.setChecked((Settings.System.getInt(resolver,
-                Settings.System.BATTERY_BAR_ENABLED, 0)) == 1);
-        mBatteryBar.setOnPreferenceChangeListener(this);
-
-        mStatusbarDualRowMode = (SystemSettingListPreference) findPreference(DUAL_STATUSBAR_ROW_MODE);
-        int statusbarDualRowMode = Settings.System.getIntForUser(getActivity().getContentResolver(),
-                Settings.System.STATUSBAR_DUAL_ROW, 0, UserHandle.USER_CURRENT);
-        mStatusbarDualRowMode.setValue(String.valueOf(statusbarDualRowMode));
-        mStatusbarDualRowMode.setSummary(mStatusbarDualRowMode.getEntry());
-        mStatusbarDualRowMode.setOnPreferenceChangeListener(this);
-
-        handleDataUsePreferences();
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        handleDataUsePreferences();
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        handleDataUsePreferences();
-    }
-
-    private void handleDataUsePreferences() {
-
-        int dualRowMode = Settings.System.getInt(getActivity().getContentResolver(),
-                Settings.System.STATUSBAR_DUAL_ROW, 0);
-
-        if (dualRowMode == 3) {
-            mDualRowDataUsageMode = (SystemSettingListPreference) findPreference(DUAL_ROW_DATAUSAGE);
-            int dualRowDataUsageMode = Settings.System.getIntForUser(getActivity().getContentResolver(),
-                    Settings.System.DUAL_ROW_DATAUSAGE, 0, UserHandle.USER_CURRENT);
-            mDualRowDataUsageMode.setValue(String.valueOf(dualRowDataUsageMode));
-            mDualRowDataUsageMode.setSummary(mDualRowDataUsageMode.getEntry());
-            mDualRowDataUsageMode.setOnPreferenceChangeListener(this);
-            mDualRowDataUsageMode.setVisible(true);
-        } else {
-            mDualRowDataUsageMode = (SystemSettingListPreference) findPreference(DUAL_ROW_DATAUSAGE);
-            if(mDualRowDataUsageMode != null) {
-               mDualRowDataUsageMode.setVisible(false);
-            }
-        }
     }
 
     @Override
@@ -149,26 +96,6 @@ public class StatusBar extends SettingsPreferenceFragment implements
             boolean value = (Boolean) newValue;
             Settings.System.putInt(getActivity().getContentResolver(),
                     Settings.System.STATUS_BAR_CLOCK, value ? 1 : 0);
-            return true;
-        } else if (preference == mBatteryBar) {
-            boolean value = (Boolean) newValue;
-            Settings.System.putInt(getActivity().getContentResolver(),
-                    Settings.System.BATTERY_BAR_ENABLED, value ? 1 : 0);
-            return true;
-        } else if (preference == mStatusbarDualRowMode) {
-            int statusbarDualRowMode = Integer.parseInt((String) newValue);
-            int statusbarDualRowModeIndex = mStatusbarDualRowMode.findIndexOfValue((String) newValue);
-            Settings.System.putInt(getActivity().getContentResolver(),
-                    Settings.System.STATUSBAR_DUAL_ROW, statusbarDualRowMode);
-            mStatusbarDualRowMode.setSummary(mStatusbarDualRowMode.getEntries()[statusbarDualRowModeIndex]);
-            handleDataUsePreferences();
-            return true;
-        } else if (preference == mDualRowDataUsageMode) {
-            int dualRowDataUsageMode = Integer.parseInt((String) newValue);
-            int dualRowDataUsageModeIndex = mDualRowDataUsageMode.findIndexOfValue((String) newValue);
-            Settings.System.putInt(getActivity().getContentResolver(),
-                    Settings.System.DUAL_ROW_DATAUSAGE, dualRowDataUsageMode);
-            mDualRowDataUsageMode.setSummary(mDualRowDataUsageMode.getEntries()[dualRowDataUsageModeIndex]);
             return true;
         }
         return false;
